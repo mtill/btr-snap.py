7d6318f3f38c6fe2481e0358f9102955c1984044
you@example.com <you@example.com>
2022-12-30 08:08:42 +0100

    minor

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # btr-snap.py
 minimalist tool to create btrfs snapshots automatically
 
-## configuration
+## usage
+```
+usage: storybook [-h] [--config CONFIG] --task TASK [--dry_run]
 
+optional arguments:
+  -h, --help       show this help message and exit
+  --config CONFIG  path to config file
+  --task TASK      task to execute (e.g., "hourly"/"monthly" etc.)
+  --dry_run        perform dry run
+```
+
+## configuration
 /etc/btr-snap.conf:
 ```
 [
 
     {"subvolume": "/mnt/data/samba/server",
-     "rules": {
+     "tasks": {
          "hourly": {"preserve": 12},
          "weekly": {"preserve": 4},
          "monthly": {"preserve": 6}
@@ -16,7 +26,7 @@ minimalist tool to create btrfs snapshots automatically
     },
 
     {"subvolume": "/mnt/data/samba/media",
-     "rules": {
+     "tasks": {
          "hourly": {"preserve": 12},
          "weekly": {"preserve": 4},
          "monthly": {"preserve": 6}
@@ -28,10 +38,10 @@ minimalist tool to create btrfs snapshots automatically
 
 Append to /etc/crontab:
 ```
-0 *     * * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --rule hourly
-0 2     * * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --rule daily
-30 2    * * 0   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --rule weekly
-0 3     1 * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --rule monthly
-30 3    1 1 *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --rule yearly
+0 *     * * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --task hourly
+0 2     * * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --task daily
+30 2    * * 0   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --task weekly
+0 3     1 * *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --task monthly
+30 3    1 1 *   root    /root/btr-snap.py/btr-snap.py --config /etc/btr-snap.conf --task yearly
 ```
 
--- btr-snap.py
+++ btr-snap.py
@@ -9,30 +9,35 @@ import subprocess
 import json
 
 
-def snappy(theconfigs, therule, dry_run):
+def snappy(theconfigs, thetask, dry_run):
     for theconfig in theconfigs:
         print("\n== " + theconfig["subvolume"] + " ==")
-        rules = theconfig.get("rules", [])
-        if therule not in rules:   # rule not defined for this config; continuing
-            print("   nothing to do for rule \"" + therule + "\"")
+        tasks = theconfig.get("tasks", [])
+        if thetask not in tasks:   # task not defined for this config; continuing
+            print("   nothing to do for task \"" + thetask + "\"\n")
             continue
 
-        preserve = rules[therule].get("preserve", None)
+        preserve = tasks[thetask].get("preserve", None)
         if preserve is not None and preserve <= 0:
             raise RuntimeError("   preserve: should be a positive number")
 
         subvol = Path(theconfig["subvolume"])
-        snapDir = subvol / Path(theconfig.get("snapshot_dir", "_snapshots")) / therule
-        snapDir.mkdir(mode=0o771, exist_ok=True)
+        snapDir = subvol / Path(theconfig.get("snapshot_dir", "_snapshots")) / thetask
+        if not snapDir.exists():
+            print("   creating directory " + str(snapDir))
+            if not dry_run:
+                snapDir.mkdir(mode=0o771)
 
         if preserve is not None:
-            existingSnapshots = sorted(snapDir.iterdir(), key=lambda f: f.stat().st_ctime)
+            existingSnapshots = []
+            if snapDir.exists():
+                existingSnapshots = sorted(snapDir.iterdir(), key=lambda f: f.stat().st_ctime)
             numEntriesToDelete = len(existingSnapshots) - preserve + 1
             if numEntriesToDelete > 0:
                 print("   deleting " + str(numEntriesToDelete) + " older snapshots:")
                 for s in existingSnapshots[0:numEntriesToDelete]:
                     print("     deleting " + str(s))
-                    if not args.dry_run:
+                    if not dry_run:
                         delProcess = subprocess.run(["btrfs", "subvolume", "delete", str(s)], check=True, stdout=subprocess.DEVNULL)
 	
                 print()
@@ -41,15 +46,15 @@ def snappy(theconfigs, therule, dry_run):
         snapshotName = nowStr
         fullSnapDir = snapDir / snapshotName
         print("   creating snapshot: " + str(fullSnapDir))
-        if not args.dry_run:
+        if not dry_run:
             snapProcess = subprocess.run(["btrfs", "subvolume", "snapshot", "-r", theconfig["subvolume"], str(fullSnapDir)], check=True, stdout=subprocess.DEVNULL)
         print()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='storybook')
-    parser.add_argument('--config', required=True, help='path to config file')
-    parser.add_argument('--rule', required=True, help='selected rule (e.g., "hourly"/"monthly" etc.')
+    parser.add_argument('--config', default='/etc/btr-snap.conf', help='path to config file')
+    parser.add_argument('--task', required=True, help='task to execute (e.g., "hourly"/"monthly" etc.)')
     parser.add_argument('--dry_run', action='store_true', help='perform dry run')
     args = parser.parse_args()
 
@@ -59,5 +64,5 @@ if __name__ == "__main__":
     if args.dry_run:
         print("\n  || === DRY RUN === ||")
 
-    snappy(theconfigs=theconfigs, therule=args.rule, dry_run=args.dry_run)
+    snappy(theconfigs=theconfigs, thetask=args.task, dry_run=args.dry_run)
 
